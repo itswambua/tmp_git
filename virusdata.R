0b94e559c4c7ba6208c08b60e5c17babf9ca9de4
itswambua <22105680@student.westernsydney.edu.au>
2024-05-15 18:53:22 +1000

    plots in r

--- virusdata.R
+++ virusdata.R
@@ -1,7 +1,13 @@
+library(tidyverse)
+
 virus = read.csv("epiSEIHCRD_combAge.csv")
 head(virus)
 
-plot()
+## changed to date type where time 0 is 1/3/2020
+virus$t = as.Date(virus$t, origin="2020-03-01")
+virus_sub = subset(virus, subset = (t>="2020-07-01" & t<="2020-12-31"))
+
+plot(data$t, data$H)
 
 
 
